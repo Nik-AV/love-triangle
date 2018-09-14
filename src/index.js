3415de77c5179a0fd1741e3f288f34b6cba555c1
Nikita Avvakumov <na.avvakumov@gmail.com>
2018-09-15 00:56:06 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
+module.exports = function getLoveTrianglesCount(preferences = []) 
+{
   // your implementation
+ let count=0;
+ for(let i=0;i<preferences.length;i++) 
+ {
+  let a=preferences[i]-1;
+  if(a==i) continue;
+  let b=preferences[a]-1;
+  if(b==a) continue;
+  let c=preferences[b]-1;
+  if(c==b) continue;
+  if(c==i)
+  {
+    count++;
+  }
+ }
+ return count/3;
 };
